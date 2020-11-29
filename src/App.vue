1aba3b23e970887d0db83b0174bc2094cb916801
dd8888 <daviddiazh97@gmail.com>
2020-11-29 17:05:36 +0100

    feat: 🎸 input component
    added vue input component
    
    ✅ Closes: fix #13

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <div>
+    <InputText
+      typeInput="text"
+      nameInput="Email"
+      placeHolderInput="Introduzca su email"
+    />
+    <InputText
+      typeInput="password"
+      nameInput="Password"
+      placeHolderInput="Introduzca su contraseña"
+    />
+    <InputText typeInput="text" />
+  </div>
 </template>
 
 <script>
+import InputText from "./components/InputText";
 export default {
-
-}
+  name: "App",
+  components: {
+    InputText,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
