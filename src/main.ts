47db49068ae9ae012cb7b099530eaacf1744541c
dd8888 <daviddiazh97@gmail.com>
2020-12-06 19:35:26 +0100

    feat: 🎸 added routing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueRouter from "vue-router";
 import "@/assets/css/tailwind.css";
 Vue.config.productionTip = false;
 
+import Login from "./views/Login.vue";
+import Inventory from "./views/Inventory.vue";
+import NotFound from "./views/NotFound.vue";
+Vue.use(VueRouter);
+const routes = [
+  { path: "/login", component: App, name: "login" },
+  { path: "/inventory", component: Inventory, name: "inventory" }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
+
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  router,
+  el: "#app",
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent() {
+      const okRoute: any = routes.filter(route => {
+        return route.path === this.currentRoute;
+      });
+
+      return okRoute[0] !== undefined ? okRoute[0].component : NotFound;
+    }
+  },
+  render(h) {
+    return h((this as any).ViewComponent);
+  }
+});
---
+++ src/views/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div
+      class="h-screen w-screen bg-red-900 flex justify-center content-center flex-wrap"
+    >
+      <p class="font-sans text-white error-text">404</p>
+    </div>
+
+    <div
+      class="absolute w-screen bottom-0 mb-6 text-white text-center font-sans text-xl"
+    >
+      <span class="opacity-50">Llévame de vuelta a </span>
+      <a class="border-b" href="/login">chimaera.games</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default { name: "NotFound" };
+</script>
+
+<style>
+.error-text {
+  font-size: 130px;
+}
+
+@media (min-width: 768px) {
+  .error-text {
+    font-size: 220px;
+  }
+}
+</style>
