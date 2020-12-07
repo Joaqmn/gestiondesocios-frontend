039fde3e8779c28ba21406a2bcbf6999d10a86e4
joaquin <dreojmn@gmail.com>
2020-12-07 16:48:17 +0000

    feat: 🎸 Created first iteration of GameCarousel

---
+++ src/components/GameCarousel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="carouselContainer" class="wrapper">
+    <splide :options="options" @splide:moved="moved">
+      <splide-slide v-for="slide in slides" :key="slide.src">
+        <img :src="slide.src" alt="slide.alt" />
+      </splide-slide>
+    </splide>
+  </div>
+</template>
+
+<script>
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
+import { createSlides } from "../utils/slides.js";
+export default {
+  name: "GameCarousel",
+  components: {
+    Splide,
+    SplideSlide
+  },
+
+  data() {
+    return {
+      options: {
+        rewind: true,
+        gap: "1rem",
+        width: "100%",
+        heigh: "100%"
+      },
+      slides: createSlides()
+    };
+  },
+  methods: {
+    moved(splide, newIndex) {
+      console.log("moved", newIndex);
+    }
+  }
+};
+</script>
+
+<style>
+#carouselContainer {
+    width: 200px;
+    height: 150px;
+}
+</style>
