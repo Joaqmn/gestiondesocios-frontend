c0c711a90ae8e1e3e05c099c8356dd086ab8692d
dd8888 <daviddiazh97@gmail.com>
2020-12-14 17:54:25 +0100

    refactor: 💡 moved vuetailwind settings, added favicon new docs

--- src/utils/games.js
+++ src/utils/games.js
@@ -26,19 +26,4 @@ export default class Game {
       .update(`${basePath}/games/` + id, body)
       .then(response => response);
   }
-  async getGame(id) {
-    const url = "https://api.chimaera.games/api/games/" + id;
-    return await axios.get(url).then(response => response);
-  }
-
-  async updateGame(id, body) {
-    return await axios
-      .put("https://api.chimaera.games/api/games/" + id, body)
-      .then(response => response);
-  }
-  async deleteGame(id) {
-    return await axios
-      .delete("https://api.chimaera.games/api/games/" + id)
-      .then(response => response);
-  }
 }
