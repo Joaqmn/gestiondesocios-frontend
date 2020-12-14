609ae88626b2bc0ee286224e8f7f07e84e5c13d8
joaquin <dreojmn@gmail.com>
2020-12-14 12:59:05 +0000

    fix: 🐛 fixed borrowedGames util

--- src/utils/borrowedGames.js
+++ src/utils/borrowedGames.js
@@ -1,20 +1,20 @@
 import axios from "axios";
+import { basePath } from "./basePath.js";
 
-const basePath = "https://api.chimaera.games/api";
 export default class BorrowedGames {
   async get() {
     return await axios
-      .get("${basePath}/borrowedgames")
+      .get(`${basePath}/borrowedgames`)
       .then(response => response);
   }
   async insert(id, body) {
     return await axios
-      .post("${basePath}/borrowedgames/" + id, body)
+      .post(`${basePath}/borrowedgames/` + id, body)
       .then(response => response);
   }
   async delete(id) {
     return await axios
-      .delete("${basePath}/borrowedgames/" + id)
+      .delete(`${basePath}/borrowedgames/` + id)
       .then(response => response);
   }
 }
