bd8fe4498c269839f5dc16e7206533b872a1922d
joaquin <dreojmn@gmail.com>
2020-12-20 14:01:07 +0000

    fix: 🐛 fixed font imports

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -399,6 +399,7 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap");
 .cont {
   background-image: url("../assets/images/bottomWave.svg");
   background-repeat: no-repeat;
